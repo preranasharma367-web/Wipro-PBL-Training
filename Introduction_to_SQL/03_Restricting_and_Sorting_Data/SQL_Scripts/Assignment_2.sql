cf845e576adeeccdc7df6d1b7d21d638ecc88cd2
preranasharma367-web <preranasharma367@gmail.com>
2026-06-29 17:45:08 +0530

    Create Assignment_2.sql

---
+++ Introduction_to_SQL/03_Restricting_and_Sorting_Data/SQL_Scripts/Assignment_2.sql
@@ -0,0 +1,13 @@
+-- ==========================================================
+-- Wipro TalentNext PBL
+-- Module      : Introduction to SQL
+-- Topic       : Restricting and Sorting Data
+-- Assignment  : 02
+-- ==========================================================
+-- Objective:
+-- Display all unique job titles from the EMP table.
+-- ==========================================================
+
+SELECT DISTINCT JOB
+FROM EMP
+ORDER BY JOB;
