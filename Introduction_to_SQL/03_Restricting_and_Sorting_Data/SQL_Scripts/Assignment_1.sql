e5c1c23d8bea7291fd78b454d8b33aaa63c32e1c
preranasharma367-web <preranasharma367@gmail.com>
2026-06-29 10:54:13 +0530

    Update Assignment_1.sql

--- Introduction_to_SQL/03_Restricting_and_Sorting_Data/SQL_Scripts/Assignment_1.sql
+++ Introduction_to_SQL/03_Restricting_and_Sorting_Data/SQL_Scripts/Assignment_1.sql
@@ -1,7 +1,14 @@
--- ==========================================
--- Topic 03 : Restricting and Sorting Data
--- Assignment 1
--- ==========================================
+-- ==========================================================
+-- Wipro TalentNext PBL
+-- Module      : Introduction to SQL
+-- Topic       : Restricting and Sorting Data
+-- Assignment  : 01
+-- ==========================================================
+-- Objective:
+-- Display employee name, salary, and commission for employees
+-- who earn commission. Sort the result by Salary and Commission
+-- in descending order.
+-- ==========================================================
 
 SELECT ENAME, SAL, COMM
 FROM EMP
