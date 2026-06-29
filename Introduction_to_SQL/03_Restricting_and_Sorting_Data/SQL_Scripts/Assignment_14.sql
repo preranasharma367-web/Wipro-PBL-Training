9f29292d2accd8ca1b9f68761cdf8fb7505d8d76
preranasharma367-web <preranasharma367@gmail.com>
2026-06-29 22:34:31 +0530

    Create Assignment_14.sql

---
+++ Introduction_to_SQL/03_Restricting_and_Sorting_Data/SQL_Scripts/Assignment_14.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Assignment 14
+-- Topic : Restricting and Sorting Data
+-- Objective:
+-- Display employee names where the
+-- third letter is A.
+-- ============================================
+
+SELECT ENAME
+FROM EMP
+WHERE ENAME LIKE '__A%';
